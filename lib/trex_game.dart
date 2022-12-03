f95b5e0cb8902832c969b833e0a72886f5374333
Yulu Xu <xuyulu86@126.com>
2022-12-03 21:10:39 +0800

    add trex game

--- lib/trex_game.dart
+++ lib/trex_game.dart
@@ -1,3 +1,16 @@
+import 'dart:ui';
+
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 
-class TRexGame extends FlameGame {}
+class TRexGame extends FlameGame {
+  late final Image spriteImage;
+
+  @override
+  Color backgroundColor() => const Color(0xFFFFFFFF);
+
+  @override
+  Future<void>? onLoad() async {
+    spriteImage = await Flame.images.load('trex.png');
+  }
+}
